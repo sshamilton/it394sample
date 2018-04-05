a9ba855657af73235f32855d919401b831d7789a
Stephen Hamilton <stephen.hamilton@usma.edu>
2018-04-05 17:11:07 -0400

    added company and view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.template import loader
+from .models import Cadet
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    cadets = Cadet.objects.all() #Grab all cadets from database
+    context = {'cadets': cadets} #fill a context with the cadet list
+    template = loader.get_template('users/index.html') #Get the template we created
+    return HttpResponse(template.render(context, request)) #Render the template with the context
+
+def detail(request, cadet_id):
+    try:
+        cadet = Cadet.objects.get(pk=cadet_id)
+    except Cadet.DoesNotExist:
+        raise Http404("Cadet does not exist")
+    return render(request, 'users/detail.html', {'cadet':cadet})
+
 
